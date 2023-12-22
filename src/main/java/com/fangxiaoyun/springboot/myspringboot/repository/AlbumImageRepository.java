72f709b35d4831500f5c71e31bde678e4905a2fc
fangxiaoyun <fangxiaoyun@dev.local>
2023-12-22 16:29:59 +0800

    feat: 完善相册删除相片的接口

--- src/main/java/com/fangxiaoyun/springboot/myspringboot/repository/AlbumImageRepository.java
+++ src/main/java/com/fangxiaoyun/springboot/myspringboot/repository/AlbumImageRepository.java
@@ -2,11 +2,15 @@ package com.fangxiaoyun.springboot.myspringboot.repository;
 
 import com.fangxiaoyun.springboot.myspringboot.table.AlbumImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface AlbumImageRepository extends JpaRepository<AlbumImage, Long> {
     List<AlbumImage> findByUid(String uid);
 
@@ -14,5 +18,8 @@ public interface AlbumImageRepository extends JpaRepository<AlbumImage, Long> {
 
     void deleteByAlbumId(String albumId);
 
+    @Modifying(clearAutomatically = true)
+    @Query(nativeQuery = true,
+            value = "delete from album_image where album_id = :albumId and pic_id = :picId")
     void deleteByAlbumIdAndPicId(String albumId, long picId);
 }
