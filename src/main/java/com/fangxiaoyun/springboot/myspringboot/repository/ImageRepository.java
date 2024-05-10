4885bab23ae21d7a2e27a3afec80ea124d7c0c80
XiaoYun Fang <2569658002@qq.com>
2024-05-10 17:54:37 +0800

    feat: 优化相册图片压缩算法

--- src/main/java/com/fangxiaoyun/springboot/myspringboot/repository/ImageRepository.java
+++ src/main/java/com/fangxiaoyun/springboot/myspringboot/repository/ImageRepository.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface ImageRepository extends JpaRepository<Image, Long> {
     List<Image> findByUid(Long uid);
 
--- src/main/java/com/fangxiaoyun/springboot/myspringboot/repository/UserAlbumRepository.java
+++ src/main/java/com/fangxiaoyun/springboot/myspringboot/repository/UserAlbumRepository.java
@@ -3,10 +3,12 @@ package com.fangxiaoyun.springboot.myspringboot.repository;
 import com.fangxiaoyun.springboot.myspringboot.table.UserAlbum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface UserAlbumRepository extends JpaRepository<UserAlbum, Long> {
 
     List<UserAlbum> findByPhoneNumberOrderByCreateTime(String phoneNumber);
